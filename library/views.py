9fd67da5c5058bca908fae4772da771aebbbbc5c
mclbdn <michalbednar93@gmail.com>
2021-10-27 15:25:06 -0500

    Add context

--- library/views.py
+++ library/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from .models import Book
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.all()
+        return context
