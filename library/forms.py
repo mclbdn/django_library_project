02b0a78cffc981cec6748da53346acd64b292e27
mclbdn <michalbednar93@gmail.com>
2021-10-28 22:30:26 -0500

    Redo forms attrs and use semantic ui for it

--- library/forms.py
+++ library/forms.py
@@ -5,3 +5,10 @@ class BookForm(ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'year', 'page_count', 'author']
+
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'placeholder': 'Book\'s title', 'style': 'max-width: 75%;'})
+        self.fields['author'].widget.attrs.update({'placeholder': 'Author\'s full name', 'style': 'max-width: 75%;'})
+        self.fields['year'].widget.attrs.update({'placeholder': 'Year this book was originally published', 'style': 'max-width: 50%;'})
+        self.fields['page_count'].widget.attrs.update({'placeholder': 'Number of pages', 'style': 'max-width: 50%;'})
