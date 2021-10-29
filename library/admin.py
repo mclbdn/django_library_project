1cdc9299e104211a10c1091f01aaabe18072c997
mclbdn <michalbednar93@gmail.com>
2021-10-29 16:08:23 -0500

    Change admin model

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Book
 
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'author', 'year', 'page_count', 'added')
+
 # Register your models here.
 
-admin.site.register(Book)
+admin.site.register(Book, HomeAdmin)
